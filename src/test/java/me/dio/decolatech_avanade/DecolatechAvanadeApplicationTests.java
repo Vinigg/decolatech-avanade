c2d836aeba4a739fd86e4c97902a95b2a3668d04
Vinigg <wgg.vini@gmail.com>
2025-03-14 03:00:39 -0300

    initial commit

---
+++ src/test/java/me/dio/decolatech_avanade/DecolatechAvanadeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decolatech_avanade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolatechAvanadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
